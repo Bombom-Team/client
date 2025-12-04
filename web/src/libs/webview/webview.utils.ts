a75b45484bc1d2bb2cd47972856fb4e3ae69f7e2
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-12-05 00:42:46 +0900

    chore: 불필요한 warning 삭제

--- web/src/libs/webview/webview.utils.ts
+++ web/src/libs/webview/webview.utils.ts
@@ -4,7 +4,6 @@ import type { RNToWebMessage, WebToRNMessage } from '@bombom/shared/webview';
 
 export const sendMessageToRN = (message: WebToRNMessage): void => {
   if (!isWebView()) {
-    console.warn('WebView 환경이 아닙니다. 메시지가 전송되지 않습니다.');
     return;
   }
 
