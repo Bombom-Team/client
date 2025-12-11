817670b32e77c59e01168b85cb116d078a14bad8
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2025-12-11 18:56:40 +0900

    [FE][BOM-584] refactor: 버튼 컴포넌트 1차 리팩토링 (컴포넌트 리팩토링 및 사용처에 반영) (#29)
    * refactor: 버튼 컴포넌트 리팩터링
    
    - 버튼 스타일을 primary, secondary, transparent로 세분화
    
    - 버튼 콘텐츠를 text에서 children으로 확장
    
    * refactor: Button 컴포넌트 사용처들에 컴포넌트 변경사항 반영
    
    - 불필요한 스타일링을 생략하고 필요한 스타일 코드만 유지
    - variant 추가
    
    * refactor: 버튼 variant를 primary, secondary에서 filled, outlined로 변경
    
    - 스타일을 강조하는 variant명으로 변경

--- web/src/pages/login/components/LoginCard.tsx
+++ web/src/pages/login/components/LoginCard.tsx
@@ -1,5 +1,6 @@
 import { theme } from '@bombom/shared/theme';
 import styled from '@emotion/styled';
+import Button from '@/components/Button/Button';
 import { useDevice } from '@/hooks/useDevice';
 import { navigateToOAuthLogin } from '@/utils/auth';
 import { isIOS, isWebView } from '@/utils/device';
@@ -33,7 +34,7 @@ const LoginCard = () => {
         onClick={() => {
           navigateToOAuthLogin({ provider: 'google' });
         }}
-        type="button"
+        variant="outlined"
       >
         <GoogleIcon width={24} height={24} fill="black" />
         Google로 계속하기
@@ -43,7 +44,7 @@ const LoginCard = () => {
           onClick={() => {
             navigateToOAuthLogin({ provider: 'apple' });
           }}
-          type="button"
+          variant="outlined"
         >
           <AppleIcon width={24} height={24} fill="black" />
           Apple로 계속하기
@@ -134,26 +135,12 @@ const Divider = styled.div`
   );
 `;
 
-const LoginButton = styled.button`
+const LoginButton = styled(Button)`
   width: 100%;
   padding: 12px;
-  border: 2px solid ${({ theme }) => theme.colors.dividers};
-  border-radius: 8px;
   box-shadow: 0 4px 6px -1px rgb(0 0 0 / 5%);
 
-  display: flex;
-  gap: 12px;
-  align-items: center;
-  justify-content: center;
-
-  background-color: ${({ theme }) => theme.colors.white};
   font: ${({ theme }) => theme.fonts.body1};
-  text-align: center;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.primaryLight};
-    transition: background-color 0.5s ease;
-  }
 `;
 
 const Terms = styled.p`
