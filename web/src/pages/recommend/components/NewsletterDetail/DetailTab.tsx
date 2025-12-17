52b72ff1a7867a6eb46de6acbbd2407e9e76dbb7
Jeongeun Lee <leeju1009@gmail.com>
2025-12-17 23:50:10 +0900

    refactor: 뉴스레터 상세 정보 모달에서 구독 해지 버튼 제거

--- web/src/pages/recommend/components/NewsletterDetail/DetailTab.tsx
+++ web/src/pages/recommend/components/NewsletterDetail/DetailTab.tsx
@@ -1,11 +1,5 @@
 import styled from '@emotion/styled';
-import { createPortal } from 'react-dom';
 import NewsletterSubscribeGuide from './NewsletterSubscribeGuide';
-import Modal from '@/components/Modal/Modal';
-import useModal from '@/components/Modal/useModal';
-import { useUnsubscribe } from '@/pages/MyPage/hooks/useUnsubscribe';
-import NewsletterUnsubscribeModal from '@/pages/MyPage/NewsletterUnsubscribeModal';
-
 interface DetailTabProps {
   newsletterDescription: string;
   newsletterId: number;
@@ -13,59 +7,13 @@ interface DetailTabProps {
   isMobile: boolean;
 }
 
-const DetailTab = ({
-  newsletterDescription,
-  newsletterId,
-  isSubscribed,
-  isMobile,
-}: DetailTabProps) => {
-  const { selectNewsletter, confirmUnsubscribe } = useUnsubscribe();
-  const { modalRef, openModal, closeModal, isOpen } = useModal({
-    onClose: () => {
-      selectNewsletter(null);
-    },
-  });
-
-  const openUnsubscribeModal = (newsletterId: number) => {
-    selectNewsletter(newsletterId);
-    openModal();
-  };
-
-  const confirmUnsubscribeNewsletter = () => {
-    confirmUnsubscribe();
-    closeModal();
-  };
-
+const DetailTab = ({ newsletterDescription, isMobile }: DetailTabProps) => {
   return (
     <>
       <Container isMobile={isMobile}>
         <Description isMobile={isMobile}>{newsletterDescription}</Description>
-        {isSubscribed && (
-          <ButtonWrapper>
-            <UnsubscribeButton
-              onClick={() => openUnsubscribeModal(newsletterId)}
-            >
-              구독 해지
-            </UnsubscribeButton>
-          </ButtonWrapper>
-        )}
-
         {!isMobile && <NewsletterSubscribeGuide />}
       </Container>
-      {createPortal(
-        <Modal
-          modalRef={modalRef}
-          closeModal={closeModal}
-          isOpen={isOpen}
-          showCloseButton={false}
-        >
-          <NewsletterUnsubscribeModal
-            onUnsubscribe={confirmUnsubscribeNewsletter}
-            onClose={closeModal}
-          />
-        </Modal>,
-        document.body,
-      )}
     </>
   );
 };
@@ -76,20 +24,6 @@ const Container = styled.div<{ isMobile: boolean }>`
   display: flex;
   gap: ${({ isMobile }) => (isMobile ? '16px' : '24px')};
   flex-direction: column;
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const UnsubscribeButton = styled.button`
-  color: ${({ theme }) => theme.colors.textTertiary};
-  font: ${({ theme }) => theme.fonts.caption};
-
-  &:hover {
-    text-decoration: underline;
-  }
 `;
 
 const Description = styled.p<{ isMobile: boolean }>`
