7c0f62a2eb377fd5d5d6609b5d42dae419679fa9
Jeongeun Lee <leeju1009@gmail.com>
2025-12-23 04:46:07 +0900

    feat: 카운트 다운 기능을 구현한 커스텀 훅 생성

---
+++ web/src/hooks/useCountdown.ts
@@ -0,0 +1,78 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { convertMillisecondsToTime } from '@/utils/time';
+
+interface UseCountdownParams {
+  targetTime: string | Date;
+  initialTime?: string | Date;
+  onComplete?: () => void;
+  interval?: number;
+}
+
+interface Time {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  totalSeconds: number;
+}
+
+export const useCountdown = ({
+  targetTime,
+  initialTime,
+  onComplete,
+  interval = 1000,
+}: UseCountdownParams) => {
+  const [leftTime, setLeftTime] = useState<Time>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    totalSeconds: 0,
+  });
+  const [isComplete, setIsComplete] = useState(false);
+  const targetTimeMs = useRef(new Date(targetTime).getTime());
+  const getCurrentTime = useRef<(() => number) | null>(null);
+
+  const createCurrentTimeTracker = useCallback((currentTimeMs: number) => {
+    const startPerformanceTime = performance.now();
+    return () => currentTimeMs + (performance.now() - startPerformanceTime);
+  }, []);
+
+  useEffect(() => {
+    if (initialTime) {
+      const initialTimeMs = new Date(initialTime).getTime();
+      getCurrentTime.current = createCurrentTimeTracker(initialTimeMs);
+    } else {
+      getCurrentTime.current = () => Date.now();
+    }
+
+    targetTimeMs.current = new Date(targetTime).getTime();
+  }, [createCurrentTimeTracker, initialTime, targetTime]);
+
+  const updateTimeLeft = useCallback(() => {
+    if (!getCurrentTime.current) return;
+
+    const currentTime = getCurrentTime.current();
+    const timeDiff = targetTimeMs.current - currentTime;
+
+    const remainTime = convertMillisecondsToTime(timeDiff);
+    setLeftTime(remainTime);
+
+    if (remainTime.totalSeconds <= 0 && onComplete) {
+      setIsComplete(true);
+      onComplete();
+    }
+  }, [onComplete]);
+
+  useEffect(() => {
+    updateTimeLeft();
+
+    const timerId = setInterval(updateTimeLeft, interval);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [interval, updateTimeLeft]);
+
+  return { leftTime, isComplete };
+};
