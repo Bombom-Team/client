58f72e5be071817000f2fcf21a7b4ec4e48f3fd7
Jeongeun Lee <leeju1009@gmail.com>
2025-12-23 04:39:49 +0900

    feat: ms 초를 시간으로 변환하는 유틸 함수 생성

---
+++ web/src/utils/time.ts
@@ -0,0 +1,15 @@
+export const convertMillisecondsToTime = (milliseconds: number) => {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const days = Math.floor(totalSeconds / (60 * 60 * 24));
+  const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+  const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+  const seconds = totalSeconds % 60;
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+    totalSeconds,
+  };
+};
