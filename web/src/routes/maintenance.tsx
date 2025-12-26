4114c6f894ff977070a1e86bafd9d34b491fb391
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-12-26 13:36:40 +0900

    chore: landing, login robots 메타 데이터 추가

--- web/src/routes/maintenance.tsx
+++ web/src/routes/maintenance.tsx
@@ -2,6 +2,17 @@ import { createFileRoute } from '@tanstack/react-router';
 import MaintenanceCard from '@/pages/system/components/MaintenanceCard';
 
 export const Route = createFileRoute('/maintenance')({
+  head: () => ({
+    meta: [
+      {
+        name: 'robots',
+        content: 'noindex, nofollow',
+      },
+      {
+        title: '봄봄 | 회원가입',
+      },
+    ],
+  }),
   component: RouteComponent,
 });
 
