0553b6a88e8f816efcc2f385a3a067bece4a0aef
ChoiDongjun <adong0808@naver.com>
2026-01-03 18:46:08 +0900

    feat: 챌린지 목록 API 및 타입 추가

---
+++ admin/src/apis/challenges/challenges.api.ts
@@ -0,0 +1,19 @@
+import { fetcher } from '@bombom/shared/apis';
+import type { PageableResponse } from '@/apis/types/PageableResponse';
+import type { Challenge, ChallengeStatus } from '@/types/challenge';
+
+export type GetChallengesParams = {
+  status?: ChallengeStatus;
+  page?: number;
+  size?: number;
+  sort?: string[];
+};
+
+export type GetChallengesResponse = PageableResponse<Challenge>;
+
+export const getChallenges = async (params: GetChallengesParams = {}) => {
+  return fetcher.get<GetChallengesResponse>({
+    path: '/challenges',
+    query: params,
+  });
+};
