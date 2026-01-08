278c8d51bb11c96a25601d0b1b7371c2e3dc806a
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2026-01-08 09:26:59 +0900

    refactor: css로 Footer 오른쪽 정렬 적용

--- web/src/pages/challenge/index/components/ChallengeCard/CardFooter.tsx
+++ web/src/pages/challenge/index/components/ChallengeCard/CardFooter.tsx
@@ -1,17 +1,18 @@
 import styled from '@emotion/styled';
-import { Children, type PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 
 const CardFooter = ({ children }: PropsWithChildren) => {
-  const hasMultipleChildren = Children.count(children) > 1;
-
-  return <Footer hasMultipleChildren={hasMultipleChildren}>{children}</Footer>;
+  return <Footer>{children}</Footer>;
 };
 
 export default CardFooter;
 
-const Footer = styled.div<{ hasMultipleChildren: boolean }>`
+const Footer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: ${({ hasMultipleChildren }) =>
-    hasMultipleChildren ? 'space-between' : 'flex-end'};
+  justify-content: space-between;
+
+  & > :only-child {
+    margin-left: auto;
+  }
 `;
