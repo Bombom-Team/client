a9bc95a04936d9f39b948190424f815227bf3438
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2026-01-13 14:31:57 +0900

    feat: 커밍순 챌린지 카드 추가

---
+++ web/src/pages/challenge/index/components/ChallengeCard/cards/ChallengeCardComingSoon.tsx
@@ -0,0 +1,50 @@
+import CardContainer from '../CardContainer';
+import { Title, Tag, Applicant, DDay, CardDetailButton } from '../CardElements';
+import CardFooter from '../CardFooter';
+import CardHeader from '../CardHeader';
+import Flex from '@/components/Flex';
+import Text from '@/components/Text';
+import { trackEvent } from '@/libs/googleAnalytics/gaEvents';
+import { openExternalLink } from '@/utils/externalLink';
+import type { ChallengeCardProps } from '../ChallengeCard';
+
+const ChallengeCardComingSoon = (props: ChallengeCardProps) => {
+  const { participantCount, generation, startDate, title } = props;
+
+  const handleCardClick = () => {
+    trackEvent({
+      category: 'Challenge',
+      action: 'COMING_SOON 카드 클릭',
+      label: title,
+    });
+
+    openExternalLink(
+      'https://maroon-geranium-880.notion.site/2d103dcf205680dfa045d47385af3df9?source=copy_link',
+    );
+  };
+
+  return (
+    <CardContainer onClick={handleCardClick}>
+      <CardHeader>
+        <Flex direction="column" gap={8}>
+          <Title>{title}</Title>
+          <Tag>{generation}기</Tag>
+        </Flex>
+
+        <Flex align="flex-end" gap={12}>
+          {participantCount > 0 && (
+            <Applicant>신청자 {participantCount}명</Applicant>
+          )}
+          <DDay startDate={startDate} />
+        </Flex>
+      </CardHeader>
+
+      <CardFooter>
+        <Text>Coming Soon</Text>
+        <CardDetailButton>자세히 보기 →</CardDetailButton>
+      </CardFooter>
+    </CardContainer>
+  );
+};
+
+export default ChallengeCardComingSoon;
