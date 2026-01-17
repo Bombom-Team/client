995e04f099b439dc75713546d15758945b8da144
Jeongeun Lee <leeju1009@gmail.com>
2026-01-17 17:47:49 +0900

    refactor: mutation 함수에 api 함수를 바로 전달하도록 수정

--- web/src/pages/challenge/comments/hooks/useUpdateChallengeCommentMutation.ts
+++ web/src/pages/challenge/comments/hooks/useUpdateChallengeCommentMutation.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { patchChallengeComment } from '@/apis/challenge/challenge.api';
 import { toast } from '@/components/Toast/utils/toastActions';
-import type { PatchChallengeCommentParams } from '@/apis/challenge/challenge.api';
 
 interface UseUpdateChallengeCommentMutationParams {
   challengeId: number;
@@ -13,8 +12,7 @@ export const useUpdateChallengeCommentMutation = ({
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (params: PatchChallengeCommentParams) =>
-      patchChallengeComment(params),
+    mutationFn: patchChallengeComment,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['challenges', challengeId, 'comments'],
