27fc28552e14ccbb75a691fb14ef0a5113c18e06
Jeongeun Lee <leeju1009@gmail.com>
2026-02-02 01:35:00 +0900

    design: 레이아웃 설정

--- web/src/routes/challenge/$challengeId/landing.tsx
+++ web/src/routes/challenge/$challengeId/landing.tsx
@@ -2,8 +2,11 @@ import styled from '@emotion/styled';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute, useParams } from '@tanstack/react-router';
 import { queries } from '@/apis/queries';
+import { useDevice } from '@/hooks/useDevice';
 import Hero from '@/pages/challenge/landing/components/Hero';
 import Introduction from '@/pages/challenge/landing/components/Introduction';
+import LandingHeader from '@/pages/landing/components/LandingHeader';
+import type { Device } from '@/hooks/useDevice';
 
 export const Route = createFileRoute('/challenge/$challengeId/landing')({
   head: () => ({
@@ -17,6 +20,7 @@ export const Route = createFileRoute('/challenge/$challengeId/landing')({
 });
 
 function ChallengeLanding() {
+  const device = useDevice();
   const { challengeId } = useParams({
     from: '/challenge/$challengeId/landing',
   });
@@ -30,25 +34,36 @@ function ChallengeLanding() {
   }
 
   return (
-    <Container>
+    <Container device={device}>
+      <LandingHeader />
       <Hero
         challengeName={challengeInfo.name}
         generation={challengeInfo.generation}
       />
-      <Introduction
-        startDate={challengeInfo.startDate}
-        endDate={challengeInfo.endDate}
-      />
+      <Content device={device}>
+        <Introduction
+          startDate={challengeInfo.startDate}
+          endDate={challengeInfo.endDate}
+        />
+      </Content>
     </Container>
   );
 }
 
-const Container = styled.main`
+const Container = styled.main<{ device: Device }>`
   width: 100%;
   min-height: 100dvh;
+`;
+
+const Content = styled.div<{ device: Device }>`
+  padding: ${({ device }) =>
+    device === 'mobile' ? '0 20px 80px 20px' : '0 60px 240px 60px'};
 
   display: flex;
+  gap: ${({ device }) => (device === 'mobile' ? '80px' : '120px')};
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   background-color: ${({ theme }) => theme.colors.white};
 
