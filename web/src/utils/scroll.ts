14b2b4669b6dd5da06281b3e14dcabe455cee007
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2026-02-03 10:21:28 +0900

    feat: 특정 부분 스크롤 계산 로직 추가

--- web/src/utils/scroll.ts
+++ web/src/utils/scroll.ts
@@ -1,7 +1,20 @@
-export const getScrollPercent = () => {
+export const getScrollPercent = (element?: HTMLElement | null) => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  if (scrollHeight <= clientHeight) return 100;
+  let currentScroll = scrollTop;
+  let totalScroll = scrollHeight - clientHeight;
 
-  return (scrollTop / (scrollHeight - clientHeight)) * 100;
+  if (element) {
+    const { top, height } = element.getBoundingClientRect();
+    const viewportHeight = window.innerHeight;
+
+    currentScroll = top * -1;
+    totalScroll = height - viewportHeight;
+  }
+
+  if (totalScroll <= 0) return 100;
+  if (currentScroll <= 0) return 0;
+  if (currentScroll >= totalScroll) return 100;
+
+  return (currentScroll / totalScroll) * 100;
 };
