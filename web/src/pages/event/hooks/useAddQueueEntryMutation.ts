0503d9bfedf90a3aaf646a2566bb9aba87ab20f5
Jeongeun Lee <leeju1009@gmail.com>
2026-02-13 05:31:00 +0900

    feat: queue entry에 등록하는 mutation 함수 추가

---
+++ web/src/pages/event/hooks/useAddQueueEntryMutation.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { postQueueEntry } from '@/apis/event/event.api';
+import { queries } from '@/apis/queries';
+import { toast } from '@/components/Toast/utils/toastActions';
+
+interface UseAddQueueEntryMutationParams {
+  couponName: string;
+}
+
+export const useAddQueueEntryMutation = ({
+  couponName,
+}: UseAddQueueEntryMutationParams) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => postQueueEntry(couponName),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: queries.queueEntry(couponName).queryKey,
+      });
+    },
+    onError: () => {
+      toast.error('대기열 등록에 실패했습니다. 다시 시도해주세요.');
+    },
+  });
+};
