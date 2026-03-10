8e5f0e20d288ca9b92f9f33a4a251513ca5f057b
Jeongeun Lee <leeju1009@gmail.com>
2026-03-10 17:23:34 +0900

    hotfix: ios일 때 알림 탭이 보이지 않도록 수정

--- web/src/routes/_bombom/_main/my.tsx
+++ web/src/routes/_bombom/_main/my.tsx
@@ -15,6 +15,7 @@ import NotificationSettingsSection from '@/pages/MyPage/NotificationSettingsSect
 import ProfileSection from '@/pages/MyPage/ProfileSection';
 import RewardsSection from '@/pages/MyPage/RewardsSection';
 import SubscribedNewslettersSection from '@/pages/MyPage/SubscribedNewslettersSection';
+import { isIOS } from '@/utils/device';
 import type { Device } from '@/hooks/useDevice';
 import type { CSSObject, Theme } from '@emotion/react';
 import AvatarIcon from '#/assets/svg/avatar.svg';
@@ -63,7 +64,7 @@ function MyPage() {
     },
   });
 
-  const tabs = [...DEFAULT_TABS, ...WEBVIEW_TABS];
+  const tabs = isIOS() ? [...DEFAULT_TABS] : [...DEFAULT_TABS, ...WEBVIEW_TABS];
 
   if (!userInfo) return null;
 
