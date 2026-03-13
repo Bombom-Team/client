62498a3325c6fb74760106d16a28af0682ef513e
Jeongeun Lee <leeju1009@gmail.com>
2026-03-13 10:55:18 +0900

    refactor: ios일 때도 알림 설정을 할 수 있도록 탭 복원

--- web/src/routes/_bombom/_main/my.tsx
+++ web/src/routes/_bombom/_main/my.tsx
@@ -15,7 +15,7 @@ import NotificationSettingsSection from '@/pages/MyPage/NotificationSettingsSect
 import ProfileSection from '@/pages/MyPage/ProfileSection';
 import RewardsSection from '@/pages/MyPage/RewardsSection';
 import SubscribedNewslettersSection from '@/pages/MyPage/SubscribedNewslettersSection';
-import { isIOS, isWebView } from '@/utils/device';
+import { isWebView } from '@/utils/device';
 import type { Device } from '@/hooks/useDevice';
 import type { CSSObject, Theme } from '@emotion/react';
 import AvatarIcon from '#/assets/svg/avatar.svg';
@@ -64,10 +64,9 @@ function MyPage() {
     },
   });
 
-  const tabs =
-    isIOS() || !isWebView()
-      ? [...DEFAULT_TABS]
-      : [...DEFAULT_TABS, ...WEBVIEW_TABS];
+  const tabs = isWebView()
+    ? [...DEFAULT_TABS, ...WEBVIEW_TABS]
+    : [...DEFAULT_TABS];
 
   if (!userInfo) return null;
 
