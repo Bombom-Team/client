6239b094b7582fd7f6fbba436e397408c57b4038
Jeongeun Lee <leeju1009@gmail.com>
2026-03-20 18:37:42 +0900

    refactor: 공유 링크 복사에 성공하면 토스트를 띄우도록 수정

--- web/src/pages/blog/components/FloatingPostPanel.tsx
+++ web/src/pages/blog/components/FloatingPostPanel.tsx
@@ -1,5 +1,6 @@
 import FloatingActionPanel from '@/components/FloatingActionPanel/FloatingActionPanel';
 import ChevronIcon from '@/components/icons/ChevronIcon';
+import { toast } from '@/components/Toast/utils/toastActions';
 import { useDevice } from '@/hooks/useDevice';
 import { copyToClipboard } from '@/utils/copy';
 import ShareIcon from '#/assets/svg/share.svg';
@@ -7,12 +8,17 @@ import ShareIcon from '#/assets/svg/share.svg';
 const FloatingPostPanel = () => {
   const device = useDevice();
 
-  const handleScrollToTop = () => {
+  const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  const handleShareClick = () => {
-    copyToClipboard(window.location.href);
+  const copyShareLink = async () => {
+    try {
+      await copyToClipboard(window.location.href);
+      toast.success('공유 링크가 복사되었습니다.');
+    } catch {
+      toast.error('링크 복사에 문제가 발생했습니다. 다시 시도해주세요.');
+    }
   };
 
   if (device === 'mobile' || device === 'tablet') {
@@ -26,12 +32,12 @@ const FloatingPostPanel = () => {
       actions={[
         {
           icon: <ChevronIcon direction="up" width={28} height={28} />,
-          onClick: handleScrollToTop,
+          onClick: scrollToTop,
           ariaLabel: '페이지 최상단 이동',
         },
         {
           icon: <ShareIcon width={24} height={24} />,
-          onClick: handleShareClick,
+          onClick: copyShareLink,
           ariaLabel: '공유하기',
         },
       ]}
