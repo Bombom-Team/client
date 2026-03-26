30021432ff17d2386852138fd72e835f480f66bc
Jeongeun Lee <leeju1009@gmail.com>
2026-03-26 21:58:25 +0900

    feat: 공유기능 추가

--- web/src/pages/blog/components/PostDetail/ShareButton.tsx
+++ web/src/pages/blog/components/PostDetail/ShareButton.tsx
@@ -1,9 +1,20 @@
 import Button from '@/components/Button/Button';
+import { toast } from '@/components/Toast/utils/toastActions';
+import { copyToClipboard } from '@/utils/copy';
 import ShareIcon from '#/assets/svg/share.svg';
 
 const ShareButton = () => {
+  const copyShareLink = async () => {
+    try {
+      await copyToClipboard(window.location.href);
+      toast.success('공유 링크가 복사되었습니다.');
+    } catch {
+      toast.error('링크 복사에 문제가 발생했습니다. 다시 시도해주세요.');
+    }
+  };
+
   return (
-    <Button variant="outlined" onClick={() => {}}>
+    <Button variant="outlined" onClick={copyShareLink}>
       <ShareIcon width={16} height={16} />글 공유하기
     </Button>
   );
