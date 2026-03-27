41b98b0f046ea68bf4d3b7f450f45639b60de79f
jaeyoung <wodud2443@gmail.com>
2026-03-28 02:12:53 +0900

    fix: userAgent 추가하는 방식으로 변경

--- app/components/main/MainScreen.tsx
+++ app/components/main/MainScreen.tsx
@@ -156,7 +156,7 @@ export const MainScreen = () => {
         <StyledWebView
           ref={webViewRef}
           source={{ uri: ENV.webUrl }}
-          userAgent={`${navigator.userAgent} ${WEBVIEW_USER_AGENT}`}
+          applicationNameForUserAgent={WEBVIEW_USER_AGENT}
           scalesPageToFit={false}
           automaticallyAdjustsScrollIndicatorInsets={false}
           injectedJavaScript={`
