d66212eabb2806f1c0a5bb6dc265dc702dbe6867
deokbin <deokbin@naver.com>
2026-04-10 23:05:29 +0900

    feat: 800-> 700 font weight 변경

--- shared/src/theme.ts
+++ shared/src/theme.ts
@@ -2,12 +2,12 @@ const fontFamily =
   '"NanumGothic", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
 
 const fonts = {
-  heading1: `800 48px/60px ${fontFamily}`,
-  heading2: `800 34px/50px ${fontFamily}`,
-  heading3: `800 28px/38px ${fontFamily}`,
-  heading4: `800 24px/32px ${fontFamily}`,
-  heading5: `800 18px/28px ${fontFamily}`,
-  heading6: `800 16px/24px ${fontFamily}`,
+  heading1: `700 48px/60px ${fontFamily}`,
+  heading2: `700 34px/50px ${fontFamily}`,
+  heading3: `700 28px/38px ${fontFamily}`,
+  heading4: `700 24px/32px ${fontFamily}`,
+  heading5: `700 18px/28px ${fontFamily}`,
+  heading6: `700 16px/24px ${fontFamily}`,
   bodyLarge: `400 18px/28px ${fontFamily}`,
   body1: `400 16px/24px ${fontFamily}`,
   body2: `400 14px/22px ${fontFamily}`,
