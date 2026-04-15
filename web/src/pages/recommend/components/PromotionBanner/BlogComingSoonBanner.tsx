ca29f55b7e7eb98fb104d4e1d6e9c3be48b5d656
Jeongeun Lee <leeju1009@gmail.com>
2026-04-15 23:46:19 +0900

    refactor: 블로그 배너 디자인 수정

--- web/src/pages/recommend/components/PromotionBanner/BlogComingSoonBanner.tsx
+++ web/src/pages/recommend/components/PromotionBanner/BlogComingSoonBanner.tsx
@@ -2,14 +2,23 @@ import styled from '@emotion/styled';
 import { Link } from '@tanstack/react-router';
 import { useDevice } from '@/hooks/useDevice';
 import type { Device } from '@/hooks/useDevice';
+import logo from '#/assets/avif/logo.avif';
+
 const BlogComingSoonBanner = () => {
   const device = useDevice();
 
   return (
     <Container device={device} to="/blog">
       <Content device={device}>
-        <Title device={device}>Coming Soon</Title>
-        <Description device={device}>봄봄 블로그가 찾아옵니다.</Description>
+        <BannerImage
+          device={device}
+          src="/assets/png/blog-banner.png"
+          alt="Blog Banner"
+        />
+        <Logo device={device}>
+          <LogoImage src={logo} alt="logo" device={device} />
+          <Title>Blog</Title>
+        </Logo>
       </Content>
     </Container>
   );
@@ -21,33 +30,14 @@ const Container = styled(Link)<{ device: Device }>`
   overflow: hidden;
   position: relative;
   width: 100%;
-  min-height: 280px;
   padding: ${({ device }) => (device ? '32px 36px' : '24px 20px')};
-  border: 1px solid ${({ theme }) => theme.colors.primaryLight};
   border-radius: 24px;
 
   display: flex;
   align-items: center;
   justify-content: center;
 
-  background: ${({ theme }) => theme.colors.primaryInfo};
-
-  &::after {
-    position: absolute;
-    right: 4px;
-    bottom: ${({ device }) => (device === 'pc' ? '-4px' : '0')};
-
-    color: rgb(254 94 4 / 12%);
-    font-weight: 800;
-    font-size: ${({ device }) => {
-      if (device === 'pc') return '120px';
-      if (device === 'tablet') return '100px';
-      return '54px';
-    }};
-    line-height: 1;
-
-    content: 'Blog';
-  }
+  background: #f7f3e0;
 `;
 
 const Content = styled.div<{ device: Device }>`
@@ -63,24 +53,25 @@ const Content = styled.div<{ device: Device }>`
   justify-content: center;
 `;
 
-const Title = styled.h3<{ device: Device }>`
-  color: ${({ theme }) => theme.colors.black};
-  font-weight: 800;
-  font-size: ${({ device }) => {
-    if (device === 'pc') return '78px';
-    if (device === 'tablet') return '62px';
-    return '54px';
-  }};
-  line-height: 1;
-  letter-spacing: -0.05em;
-  text-align: center;
+const BannerImage = styled.img<{ device: Device }>`
+  width: 100%;
+  height: auto;
+  max-width: ${({ device }) => (device === 'pc' ? '480px' : '300px')};
 `;
 
-const Description = styled.p<{ device: Device }>`
-  color: ${({ theme }) => theme.colors.textPrimary};
-  font: ${({ theme, device }) =>
-    device === 'mobile' ? theme.fonts.body1 : theme.fonts.bodyLarge};
-  text-align: center;
+const Logo = styled.div<{ device: Device }>`
+  display: flex;
+  gap: ${({ device }) => (device === 'mobile' ? '8px' : '12px')};
+  align-items: center;
+`;
 
-  word-break: keep-all;
+const LogoImage = styled.img<{ device: Device }>`
+  width: ${({ device }) => (device === 'mobile' ? '40px' : '48px')};
+  height: ${({ device }) => (device === 'mobile' ? '40px' : '48px')};
+  border-radius: ${({ device }) => (device === 'mobile' ? '12px' : '16px')};
+`;
+
+const Title = styled.p`
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font: ${({ theme }) => theme.fonts.heading4};
 `;
