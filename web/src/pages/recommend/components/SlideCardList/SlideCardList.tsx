d11c622fa38b34e7c1cdc1d445b721fd3e095f19
Lee Jeongeun <106021313+JeLee-river@users.noreply.github.com>
2026-04-15 23:48:57 +0900

    refactor: 블로그 배너 디자인 수정 (#193)

--- web/src/pages/recommend/components/SlideCardList/SlideCardList.tsx
+++ web/src/pages/recommend/components/SlideCardList/SlideCardList.tsx
@@ -1,83 +1,18 @@
-import styled from '@emotion/styled';
-import { useState } from 'react';
 import BlogComingSoonBanner from '../PromotionBanner/BlogComingSoonBanner';
-import PromotionBanner from '../PromotionBanner/PromotionBanner';
-import QRCodeModal from '../QRCodeModal/QRCodeModal';
 import { Carousel } from '@/components/Carousel/Carousel';
-import useModal from '@/components/Modal/useModal';
-import { useDevice } from '@/hooks/useDevice';
-import type { StoreType } from '../PromotionBanner/PromotionBanner.types';
-
-const BANNERS = [
-  {
-    src: '/assets/avif/banner_1.avif',
-    alt: '디그, 디그와 함께 하세요. 누구보다 쉽게 핵심을 디깅해 드릴게요',
-  },
-  {
-    src: '/assets/avif/banner_2.avif',
-    alt: '캐릿, 트렌드 당일 배송, 렛츠 캐릿',
-  },
-  {
-    src: '/assets/avif/banner_3.avif',
-    alt: '데일리 바이트, 놓치면 안 될 비즈니스, 경제 이슈 한 눈에 보기',
-  },
-  {
-    src: '/assets/avif/banner_4.avif',
-    alt: '부딩, 구독형 부동산 뉴스레터',
-  },
-];
 
 const SlideCardList = () => {
-  const device = useDevice();
-  const { modalRef, closeModal, isOpen, openModal } = useModal();
-  const [storeType, setStoreType] = useState<StoreType | null>(null);
-
-  const handleOpenModal = (type: StoreType) => {
-    setStoreType(type);
-    openModal();
-  };
-
-  const handleCloseModal = () => {
-    setStoreType(null);
-    closeModal();
-  };
-
   return (
     <>
-      <Carousel.Root loop autoPlay>
+      <Carousel.Root>
         <Carousel.Slides>
           <Carousel.Slide>
             <BlogComingSoonBanner />
           </Carousel.Slide>
-          {device === 'pc' && (
-            <Carousel.Slide>
-              <PromotionBanner openModal={handleOpenModal} />
-            </Carousel.Slide>
-          )}
-          {BANNERS.map(({ src, alt }, index) => (
-            <Carousel.Slide key={`banner-${index}`}>
-              <Banner src={src} alt={alt} />
-            </Carousel.Slide>
-          ))}
         </Carousel.Slides>
-        <Carousel.NavButtons />
       </Carousel.Root>
-      <QRCodeModal
-        modalRef={modalRef}
-        isOpen={isOpen}
-        onClose={handleCloseModal}
-        storeType={storeType}
-      />
     </>
   );
 };
 
 export default SlideCardList;
-
-const Banner = styled.img`
-  width: 100%;
-  height: 280px;
-
-  object-fit: contain;
-  object-position: center;
-`;
