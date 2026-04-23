169ea27843a4b27628b6b89bf4064541ee0ccd95
Jeongeun Lee <leeju1009@gmail.com>
2026-04-24 06:45:03 +0900

    feat: 매일메일 패키지 세팅

---
+++ maeil-mail/src/main.tsx
@@ -0,0 +1,13 @@
+import { theme } from '@bombom/shared/core';
+import { ThemeProvider } from '@emotion/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+);
