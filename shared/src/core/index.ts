fcfabd9ed12c34314dd088cf462a8d78ab8070d6
Jeongeun Lee <leeju1009@gmail.com>
2026-04-24 05:53:42 +0900

    refactor: shared 패키지에 공통 컴포넌트 이동

--- shared/src/core/index.ts
+++ shared/src/core/index.ts
@@ -1,3 +1,7 @@
 export * from './theme';
 export * from './app';
+export * from './env';
+export * from './apis';
+export * from './utils';
+export * from './types';
 export type * from './webview';
---
+++ shared/src/ui-web/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { default as Flex } from './Flex';
+export type { FlexProps } from './Flex';
+
+export { default as Text } from './Text';
+export type { TextProps } from './Text';
