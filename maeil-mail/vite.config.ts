cf019f6d43e99b8c826f28acaca2b8d936ceb3ee
Jeongeun Lee <leeju1009@gmail.com>
2026-04-24 14:42:00 +0900

    chore: 번들러가 변경할 경로 수정

--- maeil-mail/vite.config.ts
+++ maeil-mail/vite.config.ts
@@ -1,8 +1,16 @@
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '#': path.resolve(__dirname, 'public'),
+      '@bombom/shared/env': path.resolve(__dirname, 'src/env.ts'),
+    },
+  },
   plugins: [
     tanstackRouter({
       target: 'react',
