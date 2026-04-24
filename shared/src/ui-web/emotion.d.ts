5dbe41d2715989e93f37e1341073f2298d72f89e
Jeongeun Lee <leeju1009@gmail.com>
2026-04-24 15:26:44 +0900

    chore: tsconfig 설정을 하나로 통합

--- shared/src/ui-web/emotion.d.ts
+++ shared/src/ui-web/emotion.d.ts
@@ -1,5 +1,5 @@
 import '@emotion/react';
-import type { AppTheme } from './src/core/theme';
+import type { AppTheme } from '../core/theme';
 
 declare module '@emotion/react' {
   export interface Theme extends AppTheme {}
