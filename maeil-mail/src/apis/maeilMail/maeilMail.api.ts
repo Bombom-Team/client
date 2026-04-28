884443bfe2d8dc06db1c0e3c9f5a8fe3c31c3b76
deokbin <deokbin@naver.com>
2026-04-28 21:34:14 +0900

    feat: maeil-mail 조회 관련 api 추가

---
+++ maeil-mail/src/apis/maeilMail/maeilMail.api.ts
@@ -0,0 +1,31 @@
+import { fetcher } from '@bombom/shared/apis';
+
+export type GetMaeilMailAnswerParams = {
+  contentId: string;
+};
+
+export type GetMaeilMailAnswerResponse = {
+  title: string;
+  answer: string;
+};
+
+export type GetMaeilMailMyAnswerResponse = {
+  title: string;
+  answer: string;
+};
+
+export const getMaeilMailAnswer = async ({
+  contentId,
+}: GetMaeilMailAnswerParams) => {
+  return await fetcher.get<GetMaeilMailAnswerResponse>({
+    path: `/maeil-mail/${contentId}/answer`,
+  });
+};
+
+export const getMaeilMailMyAnswer = async ({
+  contentId,
+}: GetMaeilMailAnswerParams) => {
+  return await fetcher.get<GetMaeilMailMyAnswerResponse>({
+    path: `/maeil-mail/${contentId}/answer/me`,
+  });
+};
