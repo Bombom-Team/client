5c85fe0c0b597b757e2a954950e78bba4010cd66
Jeongeun Lee <leeju1009@gmail.com>
2026-05-01 01:23:01 +0900

    fix: 린트 에러 수정

--- shared/src/ui-web/icons/ChevronIcon.tsx
+++ shared/src/ui-web/icons/ChevronIcon.tsx
@@ -42,6 +42,7 @@ const ChevronIcon = ({
         viewBox="0 -1000 960 960"
         width={width}
         fill={color}
+        {...props}
       >
         <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
       </svg>
