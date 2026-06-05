dbc96ff11637c37b1978ea51f36ca6f1375c780f
Jeongeun Lee <leeju1009@gmail.com>
2026-06-05 09:37:03 +0900

    feat: 매일메일 구독 취소/수정 훅 추가

---
+++ web/src/pages/my-page/hooks/useRemoveMaeilMailSubscriptionMutations.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { queries } from '@/apis/queries';
+import { deleteNativeMaeilMailSubscription } from '@/apis/subscriptions/subscriptions.api';
+import { toast } from '@/components/Toast/utils/toastActions';
+
+export const useRemoveMaeilMailSubscriptionMutations = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteNativeMaeilMailSubscription,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: queries.nativeMaeilMailSubscription().queryKey,
+      });
+      toast.success('구독이 해지되었습니다.');
+    },
+    onError: () => toast.error('구독 해지에 실패했습니다. 다시 시도해주세요.'),
+  });
+};
