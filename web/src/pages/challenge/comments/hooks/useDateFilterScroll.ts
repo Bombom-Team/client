f5d49a93cea2b7ec6a2a1806e06a2ceefc3cc21d
Jeongeun Lee <leeju1009@gmail.com>
2026-08-11 22:57:55 +0900

    refactor: 스크롤 동작을 자연스럽게 수정

--- web/src/pages/challenge/comments/hooks/useDateFilterScroll.ts
+++ web/src/pages/challenge/comments/hooks/useDateFilterScroll.ts
@@ -67,7 +67,11 @@ export const useDateFilterScroll = ({
       `[data-date="${selectedDate}"]`,
     );
 
-    selectedTab?.scrollIntoView({ inline: 'nearest' });
+    selectedTab?.scrollIntoView({
+      behavior: 'smooth',
+      inline: 'nearest',
+      block: 'nearest',
+    });
   }, [selectedDate]);
 
   return { scrollRef, canScrollLeft, canScrollRight, scrollDateFilter };
