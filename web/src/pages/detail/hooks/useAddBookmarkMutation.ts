ddbc961f90dd92abba46a86a6d7ed96c3501b175
이상희 <sang6299@naver.com>
2026-09-01 22:28:59 +0900

    feat: 읽음 및 북마크 상태 변경 시 아티클 캐시 즉시 패치 적용

--- web/src/pages/detail/hooks/useAddBookmarkMutation.ts
+++ web/src/pages/detail/hooks/useAddBookmarkMutation.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateArticleBookmarkStatus } from '@/apis/articles/articles.cache';
 import { postBookmark } from '@/apis/bookmark/bookmark.api';
 import { queries } from '@/apis/queries';
 
@@ -8,6 +9,7 @@ const useAddBookmarkMutation = ({ articleId }: { articleId: number }) => {
   return useMutation({
     mutationFn: () => postBookmark({ articleId }),
     onSuccess: () => {
+      updateArticleBookmarkStatus(queryClient, articleId, true);
       queryClient.invalidateQueries({
         queryKey: queries.articleBookmarkStatus({ articleId }).queryKey,
       });
